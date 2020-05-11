90ad4b7a0e15419c958b8d1ccb941b7e7af4af60
tangudu raviteja <tangudu.raviteja2001@gmail.com>
2020-05-11 13:05:03 +0530

    deleted files

--- html/mysite/polls/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class student(models.Model):
-    sid = models.IntegerField()
-    firstname = models.CharField(max_length=100)
-    lastname = models.CharField(max_length=100)
--- html/mysite/polls/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('register/', views.index, name='index'),
-    path('<int:id>/', views.fetchDetails),
-]
